20eb7a63fb756bd87650f5a265c5a3afd8b40d4c
Binny V A <binnyva@gmail.com>
2018-12-05 16:41:42 +0530

    Changed Donut database to new one

--- models/CFR_Participation.php
+++ models/CFR_Participation.php
@@ -12,29 +12,11 @@ class CFR_Participation {
 		if(!$user_ids) return array();
 		global $year, $config;
 
-		$donut_sql = new Sql($config['db_host'], $config['db_user'], $config['db_password'], 'makeadiff_cfrapp');
-		$donuts = $donut_sql->getById("SELECT U.madapp_user_id AS id, COALESCE(COUNT(D.id),0) AS count, COALESCE(SUM(D.donation_amount),0) AS total
-			FROM users U 
-			INNER JOIN donations D ON D.fundraiser_id=U.id
-			WHERE U.madapp_user_id IN (" . implode(",", $user_ids) . ") AND U.is_deleted=0 AND D.created_at > '2017-08-01 00:00:00'
-			GROUP BY D.fundraiser_id");
-
-		$externals = $donut_sql->getById("SELECT U.madapp_user_id AS id, COALESCE(COUNT(D.id),0) AS count, COALESCE(SUM(D.amount),0) AS total
-			FROM users U 
-			INNER JOIN external_donations D ON D.fundraiser_id=U.id
-			WHERE U.madapp_user_id IN (" . implode(",", $user_ids) . ") AND U.is_deleted=0 AND D.created_at > '2017-08-01 00:00:00'
-			GROUP BY D.fundraiser_id");
-
-		foreach ($externals as $user_id => $row) {
-			if(!empty($donuts[$user_id])) {
-				$donuts[$user_id]['count'] += $row['count'];
-				$donuts[$user_id]['total'] += $row['total'];
-			} else {
-				$donuts[$user_id]['count'] = $row['count'];
-				$donuts[$user_id]['total'] = $row['total'];
-			}
-
-		}
+		$donuts = $this->sql->getById("SELECT U.id, COALESCE(COUNT(D.id), 0) AS count , COALESCE(SUM(D.amount),0) AS total
+			FROM User U
+			INNER JOIN Donut_Donation D ON D.fundraiser_user_id=U.id
+			WHERE U.id IN (" . implode(",", $user_ids) . ") AND D.added_on > '$year-06-01 00:00:00'
+			GROUP BY U.id");
 
 		return $donuts;
 	}
